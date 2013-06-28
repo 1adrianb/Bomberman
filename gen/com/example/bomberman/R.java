0a44be2988688de865d99bce426af0f63c20947a
Irasoc Alexandru <alexirasoc@gmail.com>
2013-06-28 14:36:39 +0300

    movement buttons 3

--- gen/com/example/bomberman/R.java
+++ gen/com/example/bomberman/R.java
@@ -21,10 +21,16 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int bomberman=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int invincible_wall=0x7f020002;
-        public static final int road=0x7f020003;
+        public static final int b=0x7f020000;
+        public static final int bomb=0x7f020001;
+        public static final int bomberman=0x7f020002;
+        public static final int down=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int invincible_wall=0x7f020005;
+        public static final int left=0x7f020006;
+        public static final int right=0x7f020007;
+        public static final int road=0x7f020008;
+        public static final int up=0x7f020009;
     }
     public static final class id {
         public static final int action_settings=0x7f080003;
